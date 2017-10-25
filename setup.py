9fea8860c170f49a93e10f2d8550222f4943c3a2
GUERIN François <fguerin@ville-tourcoing.fr>
2017-10-25 10:39:14 +0200

    Adds CI for django-auth-ldap-ad

--- setup.py
+++ setup.py
@@ -1,111 +1,43 @@
 # -*- coding: utf-8 -*-
-"""
-Setup for the :mod:`auth_ldap_ad` application
-"""
-from __future__ import print_function
-import os
+from __future__ import unicode_literals
 import sys
-from setuptools import setup, find_packages
-from distutils.util import convert_path
-from fnmatch import fnmatchcase
+import os
+from distutils.core import setup
+from setuptools import find_packages
 
-standard_exclude = ('*.py', '*.pyc', '*$py.class', '*~', '.*', '*.bak')
-standard_exclude_directories = ('.*', 'CVS', '_darcs', './build',
-                                './dist', 'EGG-INFO', '*.egg-info')
+# Loads the version from package
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+from django_ldap_ad import __version__ as version
 
+setup(
+    name='django-auth-ldap-ad',
+    version=version,
+    description='An application providing some template tags to add buttons to pages',
+    author='François GUÉRIN',
+    author_email='fguerin@ville-tourcoing.fr',
+    url='https://github.com/frague59/django-auth-ldap-ad',
+    license='BSD',
+    keywords=['Django', 'LDAP', 'Active Directory'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Framework :: Django :: 1.9',
+                 'Framework :: Django :: 1.10',
+                 'Framework :: Django :: 1.11',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python',
+                 'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP',
+                 ],
+    install_requires=['Django', 'django-appconf', 'ldap3'],
 
-# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
-# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
-# Note: you may want to copy this into your setup.py file verbatim, as
-# you can't import this from another package, when you don't know if
-# that package is installed yet.
-def find_package_data(where='.', package='',
-                      exclude=standard_exclude,
-                      exclude_directories=standard_exclude_directories,
-                      only_in_packages=True,
-                      show_ignored=False):
-    """
-    Return a dictionary suitable for use in ``package_data``
-    in a distutils ``setup.py`` file.
-    The dictionary looks like::
-        {'package': [files]}
-    Where ``files`` is a list of all the files in that package that
-    don't match anything in ``exclude``.
-    If ``only_in_packages`` is true, then top-level directories that
-    are not packages won't be included (but directories under packages
-    will).
-    Directories matching any pattern in ``exclude_directories`` will
-    be ignored; by default directories with leading ``.``, ``CVS``,
-    and ``_darcs`` will be ignored.
-    If ``show_ignored`` is true, then all the files that aren't
-    included in package data are shown on stderr (for debugging
-    purposes).
-    Note patterns use wildcards, or can be exact paths (including
-    leading ``./``), and all searching is case-insensitive.
-    """
+    # Source files
+    packages=find_packages('.'),
 
-    out = {}
-    stack = [(convert_path(where), '', package, only_in_packages)]
-    while stack:
-        where, prefix, package, only_in_packages = stack.pop(0)
-        for name in os.listdir(where):
-            fn = os.path.join(where, name)
-            if os.path.isdir(fn):
-                bad_name = False
-                for pattern in exclude_directories:
-                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
-                        bad_name = True
-                        if show_ignored:
-                            print("Directory %s ignored by pattern %s" % (fn, pattern), file=sys.stderr)
-                        break
-                if bad_name:
-                    continue
-                if os.path.isfile(os.path.join(fn, '__init__.py')) and not prefix:
-                    if not package:
-                        new_package = name
-                    else:
-                        new_package = package + '.' + name
-                    stack.append((fn, '', new_package, False))
-                else:
-                    stack.append((fn, prefix + name + '/', package, only_in_packages))
-            elif package or not only_in_packages:
-                # is a file
-                bad_name = False
-                for pattern in exclude:
-                    if fnmatchcase(name, pattern) or fn.lower() == pattern.lower():
-                        bad_name = True
-                        if show_ignored:
-                            print("File %s ignored by pattern %s" % (fn, pattern), file=sys.stderr)
-                        break
-                if bad_name:
-                    continue
-                out.setdefault(package, []).append(prefix + name)
-    return out
+    # Includes static files
+    include_package_data=True,
+    packages_data={'docs': ["*", ],
+                   'static': ["*", ],
+                   'templates': ["*", ]},
 
-# Main setup function
-setup(name='django-auth-ldap-ad',
-      version='0.1.0a2',
-      description='Authentication backend for django applications through Microsoft Active Directory',
-      packages=find_packages(),
-      package_data=find_package_data(),
-      author='François GUÉRIN',
-      author_email='frague59@gmail.com',
-      license='Modified BSD',
-      url='https://gitlab.com/frague59/django-turnit',
-      download_url='https://gitlab.com/frague59/django-turnit/repository/archive.zip?ref=master',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
-                   'Framework :: Django',
-                   'Topic :: Internet :: WWW/HTTP',],
-      keywords=['django', 'Active Directory'],
-      install_requires=[
-          'docutils>=0.3',  # docutils inclusion for .rst files
-          'django>=1.8',  # django inclusion, tested with 1.8/1.9 only
-          'django-appconf>=1.0',  # Application config
-      ],
-      )
+)
+
